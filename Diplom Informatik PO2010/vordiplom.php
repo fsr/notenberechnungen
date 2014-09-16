7b94b25c5639eacbbad78fea9d61f0fdb94b6aa2
Sascha Peukert <sascha.peukert@gmail.com>
2014-09-16 14:10:16 +0200

    CSS ueberall eingebunden

--- Diplom Informatik PO2010/vordiplom.php
+++ Diplom Informatik PO2010/vordiplom.php
@@ -3,6 +3,7 @@
         <head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <title>Berechnung Vordiplom</title>
+	<link rel="stylesheet" type="text/css" href="../resourcen/style.css"> 
         </head>
 <body>
 
--- Master Medieninformatik PO2010/abschlussnote.php
+++ Master Medieninformatik PO2010/abschlussnote.php
@@ -3,6 +3,7 @@
         <head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <title>Berechnung Abschlussnote</title>
+	<link rel="stylesheet" type="text/css" href="../resourcen/style.css"> 
         </head>
 <body>
 
